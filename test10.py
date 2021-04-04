8e8db422de298aecb562f09eac437d5c91f0dd41
Aditya <65101420+Adityauh@users.noreply.github.com>
2021-04-04 18:50:04 -0500

    Add files via upload

---
+++ test10.py
@@ -0,0 +1,11 @@
+num1 = input(" what is the the first number ?")
+num2 = input("what is the second number ?")
+
+if num1 > num2:
+    print("number 1 is greater than munber 2")
+elif num1 < num2:
+    print("number1 is smaller than number 2")
+else:
+    print("Both number are same")
+    
+    
